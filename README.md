421c62fb9d97fddce161cd217e56d8788c66a326
Maxime Broussart <72034687+maxbroubrou@users.noreply.github.com>
2023-02-24 21:13:31 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# <Platform Name>
+
+## Overview
+
+<Platform project description>
+
+## Clone the code
+
+To clone the project :
+
+1. clone GOSAI repository `git clone https://github.com/GOSAI-DVIC/gosai`
+2. clone <Platform Name> platform
+`cd gosai`
+`git clone https://github.com/GOSAI-DVIC/<repository-name> home`
+
+3. Install the requires librairies `make install`
+
+
+## Launch
+
+Simply enter `make stop && make boot` on the terminal
---
+++ apps/template_app/display.js
@@ -0,0 +1,30 @@
+// import {className} from "./components/file.js";
+
+export const template_app = new p5((sketch) => {
+    sketch.name = "<template_app>";
+    sketch.activated = false;
+
+    sketch.preload = () => {
+        // Load assets (images, sounds, fonts, etc.)
+    };
+
+    sketch.set = (width, height, socket) => {
+        sketch.selfCanvas = sketch
+            .createCanvas(width, height, sketch.WEBGL)
+            .position(0, 0);
+        sketch.activated = true;
+        // Set up your app here
+        socket.on("<event_data_name>", (data) => null); //replace null by a data update function
+    };
+
+    sketch.resume = () => {};
+    sketch.pause = () => {};
+    sketch.update = () => {};
+
+    sketch.windowResized = () => resizeCanvas(windowWidth, windowHeight);
+
+    sketch.show = () => {
+        sketch.clear();
+        //Draw here
+    };
+});
